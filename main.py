982bf27795ae6d117e1cbd366df4939be6bb34f4
RyuunosukeDS3 <31015675+RyuunosukeDS3@users.noreply.github.com>
2022-10-01 20:20:09 +0000

    test

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from releases_manager import ReleasesManager
 release_manager = ReleasesManager()
 repo_name = getenv("REPO_NAME")
 
+print(repo_name)
 
 def release_major():
     release_manager.create_new_release(repo_name=repo_name, major=True)
