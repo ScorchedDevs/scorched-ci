4045d605ceb94213eee488bd78c2294c62393153
matheus.miziara <matheus.miziara@senior.com.br>
2022-10-04 14:12:15 +0000

    Corrigindo logs duplicados

---
+++ config/logging_configuration.py
@@ -0,0 +1,18 @@
+# pylint: disable=too-few-public-methods
+import logging
+import sys
+
+
+
+def config_logger():
+
+    log = logging.getLogger("werkzeug")
+    log.setLevel(logging.ERROR)
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - [%(levelname)s]: %(message)s",
+        stream=sys.stdout,
+    )
+
+config_logger()
