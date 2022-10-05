9858f392a293a893ce60837829a4bfad72b0708c
matheus.miziara <matheus.miziara@senior.com.br>
2022-10-05 11:38:45 +0000

    Corrigindo import

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from os import getenv
-from app import ReleasesManager
+from app import ReleasesCreator
 from config import config_logger
 
-release_manager = ReleasesManager()
+release_manager = ReleasesCreator()
 
 config_logger()
 
